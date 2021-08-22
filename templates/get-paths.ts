e9df1ff6bb2ffc9e442a536024b3f4b6047656e6
Elton M <eltonmarku@gmail.com>
2021-08-22 20:12:49 +0200

    add dockerfile

--- templates/get-paths.ts
+++ templates/get-paths.ts
@@ -1,15 +1,17 @@
 // deno-lint-ignore-file
-import * as stdPath from "https://deno.land/std@0.97.0/path/mod.ts";
-const __dirname = stdPath.dirname(stdPath.fromFileUrl(import.meta.url));
+import dirname from "https://deno.land/x/denoname@v0.8.1/mod/dirname.ts";
+const __dirname = dirname(import.meta);
 
 export const getNavTemplatePath = (): string => {
-  return `${__dirname}/widgets/nav.eta`;
+  return `/${__dirname}/widgets/nav.eta`;
 };
 
 export const getAddItemTemplatePath = (): string => {
-  return `${__dirname}/widgets/add.eta`;
+  console.log(__dirname);
+  return `/${__dirname}/widgets/add.eta`;
 };
 
 export const getWidgetCSSFilePath = (): string => {
-  return `${__dirname}/css/widget.css`;
+  console.log(__dirname);
+  return `/${__dirname}/css/widget.css`;
 };
