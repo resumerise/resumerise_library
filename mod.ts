3f3be72851515e819aafa84edbec935f149fdf6a
Elton M <eltonmarku@gmail.com>
2021-08-22 21:12:24 +0200

    .

--- mod.ts
+++ mod.ts
@@ -4,6 +4,5 @@ export { compileHTML } from "./renderer/html.ts";
 export { compilePDF } from "./renderer/pdf.ts";
 export { compilePNG } from "./renderer/png.ts";
 export { CompileException } from "./exceptions/compile-exception.ts";
-export { mainModuleDir } from "./relative-path.ts";
 export * from "./templates/get-paths.ts";
 export * from "./models/index.ts";
--- relative-path.ts
+++
@@ -1,4 +0,0 @@
-import * as path from "https://deno.land/std@0.103.0/path/mod.ts";
-export { path };
-
-export const mainModuleDir = path.dirname(path.fromFileUrl(Deno.mainModule));
