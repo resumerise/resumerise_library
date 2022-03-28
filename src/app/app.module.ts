ddd94d0efc709e006f528b962834ffbaa43746eb
Elton M <eltonmarku@gmail.com>
2022-03-28 15:34:55 +0200

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { NgAisModule } from 'angular-instantsearch';
+
+import { AppComponent } from './app.component';
+import { RatingMenu } from './rating-menu/rating-menu.component';
+import { ResetFiltersMobile } from './reset-filters-mobile/reset-filters-mobile.component';
+import { NoResults } from './no-results/no-results.component';
+
+@NgModule({
+  declarations: [AppComponent, RatingMenu, ResetFiltersMobile, NoResults],
+  imports: [BrowserModule, NgAisModule.forRoot()],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
